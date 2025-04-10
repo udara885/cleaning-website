a4d013076f8be97ca8de3eb41b1beecb17e09f12
Udara Lakshan <ulakshan061@gmail.com>
2025-04-10 11:37:11 +0530

    Implement routing for HomePage and AboutPage in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router"
 import Navbar from "./components/Navbar"
+import AboutPage from "./pages/AboutPage"
 import HomePage from "./pages/HomePage"
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
     </>
   )
 }
