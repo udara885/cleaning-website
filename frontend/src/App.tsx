5b98647f86ed583839f6ff239a8c8a6050c69751
Udara Lakshan <ulakshan061@gmail.com>
2025-04-10 13:48:01 +0530

    Add routes for ServicesPage and GalleryPage in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router"
 import Navbar from "./components/Navbar"
 import AboutPage from "./pages/AboutPage"
 import HomePage from "./pages/HomePage"
+import ServicesPage from "./pages/ServicesPage"
+import GalleryPage from "./pages/GalleryPage"
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
       </Routes>
     </>
   )
