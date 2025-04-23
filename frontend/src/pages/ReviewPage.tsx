ada27f70ca8c50cd624df76e65811f45c8fbdbfb
Udara Lakshan <ulakshan061@gmail.com>
2025-04-23 12:37:36 +0530

    Enhance ReviewPage layout for improved responsiveness and visual appeal

--- frontend/src/pages/ReviewPage.tsx
+++ frontend/src/pages/ReviewPage.tsx
@@ -5,62 +5,70 @@ import Footer from "../components/Footer"
 
 const ReviewPage = () => {
   return (
-    <div className="h-screen w-full text-[#001E3D]">
-      <div className="relative h-[728px] w-full">
+    <div className="min-h-screen w-full overflow-x-hidden text-[#001E3D]">
+      <div className="relative h-182 w-full lg:h-115.75">
         <div className="fixed inset-0 z-0 bg-[#046BD2] bg-cover bg-center bg-no-repeat">
           <div className="absolute inset-0 bg-black/10" />
         </div>
-        <div className="font-poppins relative z-10 flex h-[728px] flex-col items-center justify-between gap-1 px-5 pt-[17.6875rem] text-white">
+        <div className="font-poppins relative z-10 flex flex-col items-center justify-between gap-1 px-5 pt-70.75 text-white lg:flex-row lg:justify-between lg:gap-11 lg:px-20 lg:pt-31.75">
           {[
-            "top-42 left-32",
-            "top-56 right-25",
-            "top-121 left-9",
-            "top-112 right-10",
+            "top-42 left-32 lg:top-40 lg:left-106.75",
+            "top-56 right-25 lg:top-26.75 lg:left-191.75",
+            "top-121 left-9 lg:top-31.75 lg:left-15.25",
+            "top-112 right-10 lg:top-57.75",
+            "hidden lg:block lg:top-99.75 lg:left-36",
+            "hidden lg:block lg:top-92.5 lg:left-134",
           ].map((className, index) => (
             <div className={`absolute ${className}`} key={index}>
               <Star />
             </div>
           ))}
-          <div className="flex flex-col items-center gap-1">
-            <h1 className="text-4xl leading-9 font-semibold uppercase">
+          <div className="flex flex-col items-center gap-1 lg:-mt-10 lg:items-start">
+            <h1 className="text-4xl/16 font-semibold whitespace-nowrap uppercase lg:text-6xl">
               Reviews
             </h1>
-            <p className="text-center text-sm font-medium capitalize">
+            <p className="text-center text-sm font-medium capitalize lg:text-left lg:text-base">
               Our clients trust us to deliver reliable, innovative cleaning
               services that ensure cleanliness, hygiene, and a fresh environment
               across industries worldwide.
             </p>
           </div>
-          <img src="/src/assets/people.png" alt="people" />
+          <img
+            src="/src/assets/people.png"
+            alt="people"
+            className="mt-20.25 lg:mt-0 lg:w-139.25"
+          />
         </div>
       </div>
-      <div className="font-poppins relative z-20 bg-white px-5">
-        <form className="pt-7 text-black">
-          <h2 className="text-center text-xl font-semibold uppercase">
+      <div className="font-poppins relative z-20 bg-white px-5 pt-7 text-black lg:px-20 lg:pt-8">
+        <form className="lg:px-60.75">
+          <h2 className="text-center text-xl font-semibold uppercase lg:text-[2.375rem]/9.5">
             Review Spotter
           </h2>
           <div className="mt-4.5 flex flex-col gap-2">
-            <div className="flex flex-col gap-1">
-              <label htmlFor="fname" className="text-sm">
-                First Name
-              </label>
-              <input
-                type="text"
-                id="fname"
-                name="fname"
-                className="rounded-md border border-[#C6C6C6] p-3 outline-none"
-              />
-            </div>
-            <div className="flex flex-col gap-1">
-              <label htmlFor="lname" className="text-sm">
-                Last Name
-              </label>
-              <input
-                type="text"
-                id="lname"
-                name="lname"
-                className="rounded-md border border-[#C6C6C6] p-3 outline-none"
-              />
+            <div className="lg:flex lg:justify-between lg:gap-5">
+              <div className="flex w-full flex-col gap-1">
+                <label htmlFor="fname" className="text-sm">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  id="fname"
+                  name="fname"
+                  className="rounded-md border border-[#C6C6C6] p-3 outline-none"
+                />
+              </div>
+              <div className="flex w-full flex-col gap-1">
+                <label htmlFor="lname" className="text-sm">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  id="lname"
+                  name="lname"
+                  className="rounded-md border border-[#C6C6C6] p-3 outline-none"
+                />
+              </div>
             </div>
             <div className="flex flex-col gap-1">
               <label htmlFor="review" className="text-sm">
@@ -105,13 +113,13 @@ const ReviewPage = () => {
             </div>
             <button
               type="submit"
-              className="mx-[4.6875rem] mt-7 rounded-4xl bg-[#046BD2] p-3 font-semibold text-white"
+              className="mx-auto mt-9 w-66.25 rounded-4xl bg-[#046BD2] p-3 font-semibold text-white lg:mt-8 lg:w-70.75"
             >
               Submit
             </button>
           </div>
         </form>
-        <div className="mt-23.5">
+        <div className="mt-23.5 lg:mt-18.75">
           {[...Array(2)].map((_, index) => (
             <div key={index}>
               <hr className="-mx-5 text-[#505050]" />
@@ -120,7 +128,7 @@ const ReviewPage = () => {
             </div>
           ))}
         </div>
-        <div className="mt-12">
+        <div className="mt-12 lg:mt-30">
           <Footer />
         </div>
       </div>
