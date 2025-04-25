025cf26b32066d28b433f584711b2e3eb5edc41c
Udara Lakshan <ulakshan061@gmail.com>
2025-04-25 12:53:11 +0530

    Add NotFoundPage component to display a user-friendly 404 message

---
+++ frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <div className="flex h-screen items-center justify-center bg-[#046BD2]">
+      <h1 className="font-poppins text-4xl font-semibold text-white">
+        Page Not Found.🙁
+      </h1>
+    </div>
+  )
+}
+export default NotFoundPage
