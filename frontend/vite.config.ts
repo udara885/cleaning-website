2c2cf3e04ffa1d344ca69c0b1b06d267a4c152bf
Udara Lakshan <ulakshan061@gmail.com>
2025-04-30 13:04:41 +0530

    Add dotenv configuration and setup proxy for API requests in Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api/v1": {
+        target: process.env.VITE_BACKEND_URL,
+      },
+    },
+  },
 })
