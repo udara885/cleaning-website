e8a74cbf3fb0a64b5d2b05d1422ebf3d67b63d9c
Udara Lakshan <ulakshan061@gmail.com>
2025-04-30 13:38:24 +0530

    Enhance ReviewCard2 component to utilize Review interface and display review details

--- frontend/src/components/ReviewCard2.tsx
+++ frontend/src/components/ReviewCard2.tsx
@@ -1,29 +1,36 @@
 import { IoMdStar } from "react-icons/io"
 
-const ReviewCard2 = () => {
+interface Review {
+  firstName: string
+  lastName: string
+  rating: number
+  comment: string
+  createdAt: string
+}
+
+const ReviewCard2 = ({ review }: { review: Review }) => {
   return (
     <div className="font-manrope mt-9 mb-12 px-4 pt-4 pb-1">
-      <div className="flex gap-4">
-        <img
-          src="/src/assets/reviewcardimg.png"
-          className="size-10 rounded-full object-cover"
-        />
-        <div className="flex flex-col gap-1">
-          <span className="font-semibold text-[#191D23]">Samantha Payne</span>
-          <span className="text-sm/4 text-[#64748B]">Traveller</span>
+      <div className="flex items-center gap-4">
+        <div className="flex size-10 items-center justify-center rounded-full bg-[#046BD2] font-bold text-white">
+          {review.firstName.split("")[0] + " " + review.lastName.split("")[0]}
+        </div>
+        <div className="flex flex-col">
+          <span className="font-semibold text-[#191D23]">
+            {review.firstName + " " + review.lastName}
+          </span>
           <div className="mt-1 flex gap-1">
-            {[...Array(5)].map((_, index) => (
+            {[...Array(review.rating)].map((_, index) => (
               <IoMdStar key={index} className="size-5 text-yellow-300" />
             ))}
           </div>
         </div>
       </div>
       <div className="flex flex-col gap-1.5">
-        <p className="font-semibold text-[#525252]">
-          We booked a 'Premium Cleaning Package' with WISDOM CLEAN, and they
-          delivered exceptional service.
-        </p>
-        <span className="text-xs text-[#64748B]">23 Nov 2021</span>
+        <p className="font-semibold text-[#525252]">{review.comment}</p>
+        <span className="text-xs text-[#64748B]">
+          {review.createdAt.split("T")[0]}
+        </span>
       </div>
     </div>
   )
