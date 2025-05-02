8f81f1dfe6a975f5c1340a97ef1a5aa16c6f7263
Udara Lakshan <ulakshan061@gmail.com>
2025-05-02 15:17:42 +0530

    Remove redundant validation checks in addReview function

--- backend/src/controller/review.controller.ts
+++ backend/src/controller/review.controller.ts
@@ -1,10 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 import Review from "../model/review.model"
 
-interface customError extends Error {
-  statusCode?: Number
-}
-
 export const getReviews = async (
   req: Request,
   res: Response,
@@ -27,16 +23,6 @@ export const addReview = async (
   next: NextFunction
 ) => {
   try {
-    if (
-      !req.body.firstName ||
-      !req.body.lastName ||
-      !req.body.comment ||
-      !req.body.rating
-    ) {
-      const error: customError = new Error("Please fill all the fields.")
-      error.statusCode = 406
-      throw error
-    }
     const review = await Review.create(req.body)
     res.status(201).json({
       success: true,
