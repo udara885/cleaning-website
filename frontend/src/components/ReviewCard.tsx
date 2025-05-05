311feb4a69d28dbf6ea8a7c61370cf281dd6abd1
Udara Lakshan <ulakshan061@gmail.com>
2025-05-05 13:40:48 +0530

    Refactor ReviewCard to use Review type and display dynamic review data

--- frontend/src/components/ReviewCard.tsx
+++ frontend/src/components/ReviewCard.tsx
@@ -1,19 +1,19 @@
 import { Star } from "lucide-react"
+import { Review } from "../types/types"
 
-const ReviewCard = () => {
+const ReviewCard = ({ review }: { review: Review }) => {
   return (
     <div className="font-manrope w-full flex-shrink-0 rounded px-4 pt-6 pb-3 shadow-sm">
-      <h3 className="font-semibold">Samantha Payne</h3>
+      <h3 className="font-semibold">{`${review.firstName} ${review.lastName}`}</h3>
       <div className="mt-2 flex gap-1">
-        {[...Array(5)].map((_, index) => (
+        {[...Array(review.rating)].map((_, index) => (
           <Star size={17} key={index} fill="yellow" stroke="yellow" />
         ))}
       </div>
-      <p className="mt-5 mb-4 text-[#525252]">
-        Your company has been great at keeping my space clean and fresh. They
-        always ensure everything is spotless and well-maintained.
-      </p>
-      <span className="text-xs text-[#64748B]">23 Nov 2021</span>
+      <p className="mt-5 mb-4 text-[#525252]">{review.comment}</p>
+      <span className="text-xs text-[#64748B]">
+        {review.createdAt.split("T")[0]}
+      </span>
     </div>
   )
 }
